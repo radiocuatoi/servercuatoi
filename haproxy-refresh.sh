f6c023527f6aa4d84ae73115fdaea833fef86138
Dung Viet Tran <dungviettran89@gmail.com>
2017-06-14 19:35:50 +0800

    Added code to refresh haproxy

---
+++ haproxy-refresh.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+jre/bin/java -cp "./app/*" -Xms32m -Xmx128m -Xss512k -server -javaagent:newrelic/newrelic.jar -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Ho_Chi_Minh -server us.cuatoi.server.proxy.HaProxy
