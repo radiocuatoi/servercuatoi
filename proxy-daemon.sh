e1da1226238a66088d2cc2ae37772711d716ea2f
radiocuatoi <radiocuatoi@gmail.com>
2017-06-14 19:00:41 +0800

    Update proxy-daemon.sh

--- proxy-daemon.sh
+++ proxy-daemon.sh
@@ -1,6 +1,7 @@
-echo "export CONFIG=$CONFIG; export PROXY_PORT=28080; cd $(pwd); ./proxy.sh > proxy.log 2>&1 " > /tmp/proxy.sh
-start-stop-daemon -p proxy.pid --stop -s 9 || true
-./proxy-shutdown.sh || true
-start-stop-daemon -p proxy.pid -m -b --start --exec /bin/sh /tmp/proxy.sh
-sleep 5s
-timeout 30m tail -f proxy.log
+export BUILD_ID=dontKillMe;
+echo "export CONFIG=$CONFIG; export PROXY_PORT=28080; export BUILD_ID=dontKillMe; cd $(pwd); ./proxy.sh > proxy.log 2>&1 " > /tmp/proxy.sh;
+start-stop-daemon -p proxy.pid --stop -s 9 || true;
+./proxy-shutdown.sh || true;
+start-stop-daemon -p proxy.pid -m -b --start --exec /bin/sh /tmp/proxy.sh;
+sleep 5s;
+timeout 1m tail -f proxy.log || true;
