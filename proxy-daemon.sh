daf28a1bb36eb3b7e0e61df2b4d368bbc8a7c607
radiocuatoi <radiocuatoi@gmail.com>
2017-06-14 19:07:13 +0800

    Create proxy-daemon.sh

--- proxy-daemon.sh
+++ proxy-daemon.sh
@@ -1,5 +1,6 @@
 export BUILD_ID=dontKillMe;
 echo "export CONFIG=$CONFIG; export PROXY_PORT=28080; export BUILD_ID=dontKillMe; cd $(pwd); ./proxy.sh > proxy.log 2>&1 " > /tmp/proxy.sh;
+cat /tmp/proxy.sh;
 start-stop-daemon -p proxy.pid --stop -s 9 || true;
 ./proxy-shutdown.sh || true;
 start-stop-daemon -p proxy.pid -m -b --start --exec /bin/sh /tmp/proxy.sh;
