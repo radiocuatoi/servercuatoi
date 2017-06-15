a59212624c742c95e151d8961703049dfd87a147
Dung Viet Tran <dungviettran89@gmail.com>
2017-06-15 10:36:38 +0800

    Added code to start tomcat in daemon

--- proxy-daemon.sh
+++ proxy-daemon.sh
@@ -1,8 +1,41 @@
+#!/usr/bin/env bash
+#Config
 export BUILD_ID=dontKillMe;
 echo "export CONFIG=$CONFIG; export PROXY_PORT=28080; export BUILD_ID=dontKillMe; cd $(pwd); ./proxy.sh > proxy.log 2>&1 " > /tmp/proxy.sh;
 cat /tmp/proxy.sh;
-start-stop-daemon -p proxy.pid --stop -s 9 || true;
-./proxy-shutdown.sh || true;
-start-stop-daemon -p proxy.pid -m -b --start --exec /bin/sh /tmp/proxy.sh;
-sleep 5s;
-timeout 1m tail -f proxy.log || true;
+
+stop() {
+    #Stop old instance
+    echo "Stopping instances..."
+    touch /tmp/proxy.shutdown
+    timeout 35s tail -f proxy.log || true;
+    start-stop-daemon -p proxy.pid --stop -s 9 || true;
+    kill -9 $(ps aux | grep "us.cuatoi.server.main.ProxyMain" | awk '{print $2}') || true;
+    echo "...done";
+}
+
+start(){
+    #Start old instance
+    echo "Starting instances..."
+    start-stop-daemon -p proxy.pid -m -b --start --exec /bin/sh /tmp/proxy.sh;
+    sleep 10s;
+    echo "Done..."
+    timeout 1m tail -f proxy.log || true;
+}
+
+case "$1" in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  restart)
+    stop
+    start
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|restart}";
+    stop
+    start
+esac;
---
+++ tomcat-daemon.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#Config
+export BUILD_ID=dontKillMe;
+echo "export CONFIG=$CONFIG; export PORT=18080; export BUILD_ID=dontKillMe; cd $(pwd); ./tomcat.sh > tomcat.log 2>&1 " > /tmp/tomcat.sh;
+cat /tmp/tomcat.sh;
+
+stop() {
+    #Stop old instance
+    echo "Stopping instances..."
+    touch /tmp/tomcat.shutdown
+    timeout 35s tail -f tomcat.log || true;
+    start-stop-daemon -p tomcat.pid --stop -s 9 || true;
+    kill -9 $(ps aux | grep "us.cuatoi.server.main.TomcatMain" | awk '{print $2}') || true;
+    echo "...done";
+}
+
+start(){
+    #Start old instance
+    echo "Starting instances..."
+    start-stop-daemon -p tomcat.pid -m -b --start --exec /bin/sh /tmp/tomcat.sh;
+    sleep 5s;
+    echo "Done..."
+    timeout 1m tail -f tomcat.log || true;
+}
+
+case "$1" in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  restart)
+    stop
+    start
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|restart}";
+    stop
+    start
+esac;
