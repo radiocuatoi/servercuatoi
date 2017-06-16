8364f3c30907108a2615777954230c86e14fbe3e
radiocuatoi <radiocuatoi@gmail.com>
2017-06-16 11:11:03 +0800

    Update run-any-where.sh

--- run-any-where.sh
+++ run-any-where.sh
@@ -15,7 +15,7 @@ fi;
 cd ${DOWNLOAD_DIR}
 rm -rf master.zip
 rm -rf servercuatoi-master
-wget https://github.com/radiocuatoi/servercuatoi/archive/master.zip
+wget -q https://github.com/radiocuatoi/servercuatoi/archive/master.zip
 unzip master.zip
 rm -rf master.zip
 cd servercuatoi-master
