36fc3fa021d46228ec101ed7529fbb12bd11b3a0
radiocuatoi <radiocuatoi@gmail.com>
2017-06-21 15:05:36 +0800

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-jre/bin/java -cp "./app/*" -Xms128m -Xmx448m -Xss512k -server -javaagent:newrelic/newrelic.jar -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Ho_Chi_Minh -server us.cuatoi.server.main.TomcatMain
+jre/bin/java -cp "./app/*" -Xms128m -Xmx400m -server -javaagent:newrelic/newrelic.jar -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Ho_Chi_Minh -server us.cuatoi.server.main.TomcatMain
