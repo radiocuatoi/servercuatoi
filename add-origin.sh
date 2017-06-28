74b94e305a90fe97773f64cde4137e7ef06f08ba
Dung Viet Tran <dungviettran89@gmail.com>
2017-06-28 15:32:47 +0800

    Add code to push to 2 repo

--- add-origin.sh
+++ add-origin.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-git remote add o1 ssh://56c1877b0c1e66fbef000200@linkproduction-cuatoi1502.rhcloud.com/~/git/linkproduction.git/
-git remote add o2 ssh://592fb0f30c1e6684160001c3@servercuatoio3-cloudcuatoi.rhcloud.com/~/git/servercuatoio3.git/
-git remote add o3 ssh://592fb2b67628e1c2ed000064@servercuatoio4-storage1687.rhcloud.com/~/git/servercuatoio4.git/
-git remote add o4 ssh://592fb2c60c1e66bc9f000111@servercuatoio5-storage1687.rhcloud.com/~/git/servercuatoio5.git/
-git remote add o5 ssh://592fb4620c1e66b81e0000d0@servercuatoio6-files1461.rhcloud.com/~/git/servercuatoio6.git/
-git remote add o6 ssh://592fb46b7628e1e2b0000076@servercuatoio7-files1461.rhcloud.com/~/git/servercuatoio7.git/
-git remote add o7 ssh://592fb68d7628e1e2b0000091@servercuatoio8-files1461.rhcloud.com/~/git/servercuatoio8.git/
-git remote add o8 ssh://592fb7620c1e66bc9f00014f@servercuatoio9-storage1687.rhcloud.com/~/git/servercuatoio9.git/
-git remote add o9 ssh://592fb92d89f5cf5f80000009@servercuatoio10-cuatoi1502.rhcloud.com/~/git/servercuatoio10.git/
-git remote add o10 ssh://592fa8ed2d5271db690001cb@servercuatoi10b-cuatoi1502.rhcloud.com/~/git/servercuatoi10b.git/
+#git remote add o1 ssh://56c1877b0c1e66fbef000200@linkproduction-cuatoi1502.rhcloud.com/~/git/linkproduction.git/
+#git remote add o2 ssh://592fb0f30c1e6684160001c3@servercuatoio3-cloudcuatoi.rhcloud.com/~/git/servercuatoio3.git/
+#git remote add o3 ssh://592fb2b67628e1c2ed000064@servercuatoio4-storage1687.rhcloud.com/~/git/servercuatoio4.git/
+#git remote add o4 ssh://592fb2c60c1e66bc9f000111@servercuatoio5-storage1687.rhcloud.com/~/git/servercuatoio5.git/
+#git remote add o5 ssh://592fb4620c1e66b81e0000d0@servercuatoio6-files1461.rhcloud.com/~/git/servercuatoio6.git/
+#git remote add o6 ssh://592fb46b7628e1e2b0000076@servercuatoio7-files1461.rhcloud.com/~/git/servercuatoio7.git/
+#git remote add o7 ssh://592fb68d7628e1e2b0000091@servercuatoio8-files1461.rhcloud.com/~/git/servercuatoio8.git/
+#git remote add o8 ssh://592fb7620c1e66bc9f00014f@servercuatoio9-storage1687.rhcloud.com/~/git/servercuatoio9.git/
+#git remote add o9 ssh://592fb92d89f5cf5f80000009@servercuatoio10-cuatoi1502.rhcloud.com/~/git/servercuatoio10.git/
+#git remote add o10 ssh://592fa8ed2d5271db690001cb@servercuatoi10b-cuatoi1502.rhcloud.com/~/git/servercuatoi10b.git/
+git remote add s1 git@github.com:radiocuatoi/servercuatoi.git
+git remote add s2 git@github.com:radiocuatoi/servercuatoi2.git
