97e95d1c3bb9b4a7e7359a01d3790f345d1914ad
radiocuatoi <radiocuatoi@gmail.com>
2017-06-29 11:06:53 +0800

    Download to tmp

--- run-any-where.sh
+++ run-any-where.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 kill -9 $(ps aux | grep "us.cuatoi.server.main.TomcatMain" | awk '{print $2}') || true
-export DOWNLOAD_DIR="$HOME";
-if [ ! -d "${DOWNLOAD_DIR}" ]; then
-    echo "${DOWNLOAD_DIR} not found. Using tmp.";
-    DOWNLOAD_DIR = "/tmp/";
-fi;
-find /tmp -ctime +1 -exec rm -rf {} +
-export DOWNLOAD_DIR="${DOWNLOAD_DIR}/servercuatoi/${RANDOM}"
+export DOWNLOAD_DIR="/tmp/servercuatoi";
+find /tmp/ -ctime +1 -exec rm -rf {} +
 echo "Downloading to ${DOWNLOAD_DIR}"
-if [ ! -d ${DOWNLOAD_DIR} ]; then
-    mkdir -p ${DOWNLOAD_DIR}
-fi;
+rm -rf ${DOWNLOAD_DIR}
+mkdir -p ${DOWNLOAD_DIR}
 cd ${DOWNLOAD_DIR}
-rm -rf master.zip
-rm -rf servercuatoi-master
 wget -q https://github.com/radiocuatoi/servercuatoi/archive/master.zip
 unzip -q master.zip
 rm -rf master.zip
