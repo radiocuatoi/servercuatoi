69fcf5931fda8ad95445858e682411048c2ff8a1
radiocuatoi <radiocuatoi@gmail.com>
2017-07-02 12:59:32 +0800

    Update run-any-where.sh

--- run-any-where.sh
+++ run-any-where.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-kill -9 $(ps aux | grep "us.cuatoi.server.main.TomcatMain" | awk '{print $2}') || true
+./tomcat-daemon.sh stop
 export DOWNLOAD_DIR="/tmp/servercuatoi";
 find /tmp/ -ctime +1 -exec rm -rf {} +
 echo "Downloading to ${DOWNLOAD_DIR}"
@@ -10,4 +10,5 @@ wget -q https://github.com/radiocuatoi/servercuatoi/archive/master.zip
 unzip -q master.zip
 rm -rf master.zip
 cd servercuatoi-master
-sh tomcat.sh
+./tomcat-daemon.sh start
+
