c3d1e110849f54e6388d7bb0961337d675903b51
radiocuatoi <radiocuatoi@gmail.com>
2017-07-02 14:28:01 +0800

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-jre/bin/java -cp "./app/*" -Xms128m -Xmx400m -Xss512k -server -javaagent:newrelic/newrelic.jar -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Ho_Chi_Minh -server us.cuatoi.server.cloud.CloudServer
+jre/bin/java -cp "./app/*" -Xms128m -Xmx396m -Xss512k -server -javaagent:newrelic/newrelic.jar -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Ho_Chi_Minh -server us.cuatoi.server.cloud.CloudServer
