290083d54af861a5f1900c40b5e6c9fcb3fcbda8
radiocuatoi <radiocuatoi@gmail.com>
2017-07-02 19:39:17 +0800

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-jre/bin/java -cp "./app/*" -Xss512k -Xms32m -Xmx256m -server -javaagent:newrelic/newrelic.jar -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Ho_Chi_Minh -server us.cuatoi.server.main.ProxyMain
+jre/bin/java -cp "./app/*" -Xss512k -Xms64m -Xmx396m -server -javaagent:newrelic/newrelic.jar -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Ho_Chi_Minh -server us.cuatoi.server.main.ProxyMain
