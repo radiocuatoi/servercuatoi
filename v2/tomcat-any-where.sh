22f41ce733cad85bfbbb8c1a8e8dd79de02ce467
dungviettran89 <dungviettran89@gmail.com>
2017-07-10 14:12:13 +0800

    Added script to start tomcat anywhere

---
+++ v2/tomcat-any-where.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+export DOWNLOAD_DIR="/tmp/servercuatoi";
+find /tmp/ -ctime +1 -exec rm -rf {} +
+echo "Downloading to ${DOWNLOAD_DIR}"
+rm -rf ${DOWNLOAD_DIR}
+mkdir -p ${DOWNLOAD_DIR}
+cd ${DOWNLOAD_DIR}
+wget -q https://github.com/radiocuatoi/servercuatoi/archive/master.zip
+unzip -q master.zip
+rm -rf master.zip
+cd servercuatoi-master/v2
+sh tomcat-2.sh
+
